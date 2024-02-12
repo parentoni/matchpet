43b03d1031ccf961cfb3e39d9dc9ddad9b66763d
arthuroi <parentoni.arthur@gmail.com>
2024-02-12 16:48:17 -0300

    feat: created admin Middleware

--- backend/src/shared/infra/http/utils/Middleware.ts
+++ backend/src/shared/infra/http/utils/Middleware.ts
@@ -2,6 +2,7 @@ import { IAuthService } from "../../../../modules/user/services/IauthService";
 import express from "express";
 import { Guard } from "../../../core/Guard";
 import { AuthenticatedRequest } from "../models/AutheticatedRequest";
+import { decode } from "node:punycode";
 export class Middleware {
   private authService: IAuthService;
 
@@ -26,11 +27,48 @@ export class Middleware {
             return res.status(401).send("Unverified account");
           }
         } else {
-          return res.status(401).send(decoded.value.error);
+          return res.status(403).send(decoded.value.error);
         }
       } else {
         return res.status(401).send(result.value);
       }
     };
+  }
+
+  /**
+   * @param {Request} req 
+   * @param {Response} res 
+   * @param {NextFunction} next 
+   *  
+   * Middleware for admin only routes. Consider admin every user with role = 10
+   * @author Arthur Parentoni Guimaraes <parentoni.arthur@gmail.com>
+   */
+  public async admin(req: express.Request, res: express.Response, next: express.NextFunction)  {
+
+    //Get bearer token value
+    const token = req.headers["authorization"]?.split(" ")[1];
+
+    const guardResult = Guard.againstNullOrUndefined(token, "token")
+
+    // if not token return missing token repsonse (401)
+    if (guardResult.isLeft()) {
+      return res.status(401)
+    }
+
+    //decode jwt
+    const decoded = await this.authService.decodeJWT(token as string) // Assumets token as string since it has been checked by guard
+    
+    //If error decoding, send error forbidden
+    if (decoded.isLeft()){
+      return res.status(403)
+    }
+
+    //If not admin
+    if (decoded.value.role !== 10) {
+      return res.status(403)
+    }
+
+    next()
+
   }
 }
