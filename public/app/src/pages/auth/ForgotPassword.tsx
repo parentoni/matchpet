1cd2db4b621e7e46fb77afcbe34baa278ca9d2c6
arthuroi <parentoni.arthur@gmail.com>
2024-02-12 23:47:15 -0300

    fix: Take out username option from login page while case insensitive fix is not possible.

--- public/app/src/pages/auth/ForgotPassword.tsx
+++ public/app/src/pages/auth/ForgotPassword.tsx
@@ -71,7 +71,7 @@ export function EmailInput ({email, setEmail, error}: {email:string, setEmail: (
   return(
     <div className="flex flex-col gap-1">
       <div className="flex gap-3 items-center text-base">
-        <label className="font-medium">Email ou nome de usuário</label>
+        <label className="font-medium">Email</label>
       </div>
       <input className={`w-full h-12 autofill:bg-primary autofill:text-white  pl-6 py-4 border  focus:border-neutral-600 rounded-md transition ring-transparent relative focus:outline-none focus:ring-primary/20 ring-4 outline-2  font-normal text-lg ${error?"border-error  bg-error/5":"border-neutral-300"}`} placeholder="nome@provedor.com" value={email} onChange={(e) => setEmail(e.target.value)}></input>
     </div>
