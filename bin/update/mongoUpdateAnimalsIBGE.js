e00e3e0808b54c4653f485c5c488292f1693a7bf
arthuroi <parentoni.arthur@gmail.com>
2024-08-02 20:02:26 -0400

    feat: production docker

--- bin/update/mongoUpdateAnimalsIBGE.js
+++ bin/update/mongoUpdateAnimalsIBGE.js
@@ -1,5 +1,5 @@
-// chatgpt GOAT
-  db.animals.aggregate([{
+db.animals.aggregate([
+    {
         $lookup: {
             from: "users",
             localField: "donator_id",
@@ -11,13 +11,24 @@
         $unwind: "$user_info"
     },
     {
-        $addFields: {
-            ibgeId: "$user_info.ibgeId"
-        }
-    },
-    {
         $project: {
-            user_info: 0
+            _id: 1,
+            name: 1,
+            age: 1,
+            image: 1,
+            created_at: 1,
+            status: 1,
+            donator_id: 1,
+            specie_id: 1,
+            description: 1,
+            traits: 1,
+            __v: 1,
+            last_modified_at: 1,
+            contact: 1,
+            sex: 1,
+            views: 1,
+            clicks: 1,
+            ibgeId: "$user_info.ibgeId"
         }
     },
     {
@@ -28,7 +39,4 @@
             whenNotMatched: "fail"
         }
     }
-])
-
-// Execute the aggregation pipeline
-
+]);
